00f5d00ce0d5969f4e48d63fca3f3a744795fb03
360matt <mattbacq2004@gmail.com>
2020-08-13 00:58:34 +0200

    Allègement class Main

--- src/main/java/fr/ulity/bot/Main.java
+++ src/main/java/fr/ulity/bot/Main.java
@@ -1,36 +1,12 @@
 package fr.ulity.bot;
 
-import fr.ulity.bot.api.CommandManager;
-import fr.ulity.bot.api.Config;
-import fr.ulity.bot.api.Lang;
-import fr.ulity.bot.commands.PingCMD;
-import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.JDABuilder;
-
 import javax.security.auth.login.LoginException;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
 public class Main {
-    public static Config config = new Config();
-    public static JDA jda;
-
-    public static void main (String[] args) throws LoginException, IOException, URISyntaxException {
-        Lang.reload();
-
-        Config langConfig = Lang.langConfigs.get(Lang.defaultLang);
-
-        for (String x : langConfig.singleLayerKeySet("internal_vars"))
-            Lang.Prepared.internalVariable.put(x, langConfig.getString("internal_vars." + x));
-
-
-        // register commands
-        CommandManager.register(new PingCMD());
-
-
-        jda = JDABuilder.createDefault(config.getString("bot.token")).build();
-        jda.addEventListener(new CommandManager());
-
+    public static void main (String[] args) throws LoginException, IOException, URISyntaxException  {
+        MainDiscordApi.start("");
+        /* start without path, in independant Bot mode */
     }
-
 }
