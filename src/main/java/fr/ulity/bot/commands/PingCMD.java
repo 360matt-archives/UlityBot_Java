b29927e0a4c17f5e5b46847a3af34eb45ca33a57
360matt <mattbacq2004@gmail.com>
2020-08-13 00:56:14 +0200

    Création d'une vrai commande Ping

--- src/main/java/fr/ulity/bot/commands/PingCMD.java
+++ src/main/java/fr/ulity/bot/commands/PingCMD.java
@@ -1,13 +1,16 @@
 package fr.ulity.bot.commands;
 
+import fr.ulity.bot.MainDiscordApi;
 import fr.ulity.bot.api.CommandBuilder;
+import fr.ulity.bot.api.Lang;
+import fr.ulity.bot.utils.Time;
 
 public class PingCMD extends CommandBuilder {
     public PingCMD() {
         name = "ping";
         level = Level.USER;
         dm = false;
-        cooldown = 5;
+        cooldown = new Time(3);
 
         aliases = new String[]{"latence"};
 
@@ -15,7 +18,17 @@ public class PingCMD extends CommandBuilder {
 
     @Override
     public void run() {
-        reply("Pong !");
+        long ping = MainDiscordApi.jda.getGatewayPing();
+        Lang.Prepared prepared;
 
+        if (ping > 250)      prepared = Lang.prepare("commands.ping.bad");
+        else if (ping > 150) prepared = Lang.prepare("commands.ping.fair");
+        else                 prepared = Lang.prepare("commands.ping.well");
+
+        prepared
+                .variable("ping", String.valueOf(MainDiscordApi.jda.getGatewayPing()))
+                .sendChannel(channel);
+
+        cooldownApplique();
     }
 }
