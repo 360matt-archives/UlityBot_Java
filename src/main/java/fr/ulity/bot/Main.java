48b3f0acc1f27dbcea9ce6ab4d001274c86bc3ea
360matt <mattbacq2004@gmail.com>
2020-08-13 18:46:12 +0200

    Created two field for get the path of workspace bot, and convert String path to File path

--- src/main/java/fr/ulity/bot/Main.java
+++ src/main/java/fr/ulity/bot/Main.java
@@ -1,12 +1,16 @@
 package fr.ulity.bot;
 
 import javax.security.auth.login.LoginException;
+import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
 public class Main {
     public static void main (String[] args) throws LoginException, IOException, URISyntaxException  {
-        MainDiscordApi.start("");
+        MainDiscordApi.start(
+                new File(Main.class.getProtectionDomain().getCodeSource().getLocation()
+                        .toURI()).getParentFile()
+        );
         /* start without path, in independant Bot mode */
     }
 }
--- src/main/java/fr/ulity/bot/MainDiscordApi.java
+++ src/main/java/fr/ulity/bot/MainDiscordApi.java
@@ -10,23 +10,26 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 
 import javax.security.auth.login.LoginException;
+import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
 public class MainDiscordApi {
-    public static Config config = new Config();
+    public static Config config;
     public static JDA jda;
+    public static File path;
+    public static String pathString;
 
-    public static void start (String path) throws LoginException, IOException, URISyntaxException {
+    public static void start (File path) throws LoginException, IOException, URISyntaxException {
+        MainDiscordApi.path = path;
+        MainDiscordApi.pathString = path.getPath();
+
+        config = new Config();
         Lang.reload();
 
-        if (!path.equals(""))
+        if (!path.equals(new File("")))
             Updater.update();
 
-        Config langConfig = Lang.langConfigs.get(Lang.defaultLang);
-
-        for (String x : langConfig.singleLayerKeySet("internal_vars"))
-            Lang.Prepared.internalVariable.put(x, langConfig.getString("internal_vars." + x));
 
 
         // register commands
@@ -36,6 +39,8 @@ public class MainDiscordApi {
         jda = JDABuilder.createDefault(config.getString("bot.token")).build();
         jda.addEventListener(new CommandManager());
     }
+
+    public static boolean isRunning () { return jda != null; }
 
     public static void registerChecker (Class checker) { CommandManager.checkers.add(checker); }
     public static void unregisterChecker (Class checker) { CommandManager.checkers.remove(checker); }
