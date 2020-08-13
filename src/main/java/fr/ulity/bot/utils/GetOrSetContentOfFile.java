bb6df5b4703ca04699a67593b924cd0e45b87db3
360matt <mattbacq2004@gmail.com>
2020-08-13 18:48:09 +0200

    Renamed + created method set(File, String) with commons-io

--- src/main/java/fr/ulity/bot/utils/GetOrSetContentOfFile.java
+++ src/main/java/fr/ulity/bot/utils/GetOrSetContentOfFile.java
@@ -1,11 +1,12 @@
 package fr.ulity.bot.utils;
 
+import org.apache.commons.io.FileUtils;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class GetContentOfFile {
+public class GetOrSetContentOfFile {
     public static String get (File file) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(file));
         StringBuilder stringBuilder = new StringBuilder();
@@ -21,5 +22,14 @@ public class GetContentOfFile {
 
         return stringBuilder.toString();
     }
+
+    public static void set(File file, String content) {
+        try {
+            FileUtils.writeStringToFile(file, content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
 
